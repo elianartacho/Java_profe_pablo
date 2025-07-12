f74cc89f4538d9dbbe94e227ca64219eda789366
elianartacho <elianartacho@gmail.com>
2025-07-13 00:06:12 +0200

    a

--- Strings/src/ejercicios/eje23.java
+++ Strings/src/ejercicios/eje23.java
@@ -2,26 +2,53 @@ package ejercicios;
 //Método que retorne cuántas palabras de menos de 3 letras tiene una frase, considerando
 // como separador de palabras únicamente el inicio de cadena, el espacio y el fin de cadena.
 
+
 public class eje23 {
     public static void main(String[] args) {
-        System.out.println(retorno("he tu sol"));
-        
+        System.out.println(retorno("he tu sol el ya vino"));
     }
 
-    public static String retorno(String frase){
-       int contador= 0;
-       String espacio = ""; 
+    public static int retorno(String frase) {
+        int contadorLetras = 0;
+        int contadorPalabras = 0;
 
-        for (int i = 0; i < frase.length() ; i++) {
-           char actual = frase.charAt(i);
-           if(i < 2){
-            contador++; 
-           }else if(espacio.equals(actual + "")){
-                i = 0;
+        for (int i = 0; i < frase.length(); i++) {
+            char let = frase.charAt(i);
+
+            if (let != ' ') {
+                contadorLetras++; // Contamos letras
+            } else {
+                if (contadorLetras > 0 && contadorLetras < 3) {
+                    contadorPalabras++; // Contamos palabra pequeña
+                }
+                contadorLetras = 0; // Reiniciamos para la siguiente palabra
             }
-        
         }
-        return "hay " + contador + " palabras de menos de tres letras";
+
+        // Comprobamos la última palabra (por si no termina en espacio)
+        if (contadorLetras > 0 && contadorLetras < 3) {
+            contadorPalabras++;
+        }
+
+        return contadorPalabras;
+    }
+}
+
+/* public class eje23 {
+    public static void main(String[] args) {
+        System.out.println(retorno("he tu sol"));
     }
 
+    public static int retorno(String frase){
+        int contador = 0;
+        String[] palabras = frase.split(" "); // Dividimos la frase por espacios
+
+        for(String palabra : palabras){
+            if(palabra.length() < 3){
+                contador++;
+            }
+        }
+        return contador;
+    }
 }
+ */
