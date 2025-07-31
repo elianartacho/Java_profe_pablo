007759099c1bbcee9ea9e0fb6ff86a7e9fa08683
elianartacho <elianartacho@gmail.com>
2025-07-31 23:35:36 +0200

    a

--- j15_registros/src/registrosdto/ClienteDTO.java
+++ j15_registros/src/registrosdto/ClienteDTO.java
@@ -1,6 +1,7 @@
 package registrosdto;
 
-//es un objeto inmutable que se usa para transportar datos y asi nos ahorramos el mantenimiento de tanto codigo
+//es un objeto inmutable que se usa para transportar datos y asi nos ahorramos
+// el mantenimiento de tanto codigo
 //ser crea un constructor con getter,equals y to string
 public record ClienteDTO(int idCliente, String apellidos, int nroCliente, String categoria) {
 
